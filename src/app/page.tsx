2ce75fe5d1c6706b07cb8cc2522c27629ec9aaac
DevAlexSanz <devalexsanz@gmail.com>
2024-08-26 17:38:52 -0600

    Fix Bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
-          Make things float in air
+          Make things float in 3D
         </CardItem>
         <CardItem
           as="p"
